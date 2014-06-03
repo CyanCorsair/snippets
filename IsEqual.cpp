fea517548e720a573c87c85446dfc68d68e3bd93
Silver <CyanCorsair@users.noreply.github.com>
2014-06-03 21:42:22 +0200

    Update IsEqual.cpp

--- IsEqual.cpp
+++ IsEqual.cpp
@@ -3,6 +3,7 @@
 //Snippet to determine if two floats are "close enough" due to C++ == operator inaccuracies
 //All credit to Donald Knuth, a famous computer scientist, who suggested the following method in his book 
 //“The Art of Computer Programming, Volume II: Seminumerical Algorithms (Addison-Wesley, 1969)”
+//file uses hungarian notation for var names
 
 bool IsEqual(double dX, double dY)
 {
