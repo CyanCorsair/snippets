7d11e552e81cd01007a4e6907226beb1c5982e4d
QawsDK <peterrasmussen@Peters-Mac-mini.local>
2014-06-20 13:27:29 +0200

    Formatted sender name

--- email_response.php
+++ email_response.php
@@ -1,5 +1,6 @@
 <?php
 if (isset($_POST['resp_email'])) {
+	$from = "Bordbestilling <bordbestilling@froggyscafe.dk>";
 	$email = $_POST['resp_email'];
 	$subject = "Tak for din reservation på Froggy's Café";
 	$message = "Vi har modtaget din reservation, og vi har bord klar til dig, som du har ønsket.\n
@@ -8,7 +9,8 @@ Vi glæder os til at se dig.\n
 Med venlig hilsen\n
 Froggy’s Cafe";
 
-	mail($email, $subject, $message);
+	$headers = 'From: ' . $from . PHP_EOL;
+	mail($email, $subject, $message, $headers);
 }
 
 ?>
